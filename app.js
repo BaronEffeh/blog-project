866a8f5089d3d0c875bc94571a8e13c8426d9174
Baron Effeh <effehbaron@gmail.com>
2023-10-24 15:19:59 +0100

    Views and partials

--- app.js
+++ app.js
@@ -10,19 +10,29 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Some key message about Yoshi and his eggs' },
+        { title: 'Mario finds stars', snippet: 'Some key message about Mario and his stars' },
+        { title: 'How to defeat browser', snippet: 'Some key message on how to defeat browser' },
+
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
 // redirects
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
-})
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Creat a new Blog' });
+});
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+    res.status(404).render('404', { title: '404' });
+});
